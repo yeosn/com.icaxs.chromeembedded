ddf1947c401b1c62ee6b64dc162f3948a6b9bc2f
yeosn <dengsenyang@163.com>
2017-06-13 15:49:43 +0800

    安装了CefSharp49

---
+++ ChromeEmbedded/MyChrome.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CefSharp;
+using CefSharp.WinForms;
+using System.Windows.Forms;
+
+namespace com.icaxs.chromeembedded
+{
+    public class MyChromium
+    {
+        /*
+         * 当前的webkit的问题：
+         *      如果在一个窗体中加载多个内容的话，则会使得内存增加过多，无法释放
+         * 解决方法：
+         *      可以将其设置为单独的程序，每次进行调用，调用前再将其关闭；
+         *      类似于通力的报表程序
+         */
+        //private ControlCollection _parentControlCollection;
+        //private CefSettings s_setting;
+        private ChromiumWebBrowser chromeBrowser;
+        //private void InitialSetting()
+        //{
+        //    if(s_setting==null)
+        //    {
+        //        s_setting = new CefSettings();
+        //        // Initialize cef with the provided settings
+        //        //Setting is initialized only once!
+        //        Cef.Initialize(s_setting);
+        //     }
+        //}
+
+        public void LoadUrl(String url)
+        {
+            chromeBrowser.Load(url);
+        }
+
+        public MyChromium(string url)
+        {
+            //InitialSetting();
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            //Setting is initialized only once!
+            Cef.Initialize(settings);
+            chromeBrowser = new ChromiumWebBrowser(url);
+        }
+
+        public Control GetBrowser()
+        {
+            return chromeBrowser;
+        }
+
+
+        public void Shutdown()
+        {
+            Cef.Shutdown();
+        }
+    }
+}
---
+++ ChromeEmbeddedTest/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using com.icaxs.chromeembedded;
+
+namespace ChromeEmbeddedTest
+{
+    public partial class Form1 : Form
+    {
+        private MyChromium browser = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            browser = new MyChromium("www.360.cn");
+
+            this.Controls.Add(browser.GetBrowser());
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            browser.Shutdown();
+        }
+    }
+}
